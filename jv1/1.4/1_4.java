2f74d6138e474d836f5e6f89c823a582ec4f621c
Krooshk <kirill.krush@mail.ru>
2025-05-27 20:30:18 +0300

    add task 1.4

---
+++ jv1/1.4/1_4.java
@@ -0,0 +1,91 @@
+// Рефлексия 1.3
+// Явно не прописывал видимость методов.
+
+    class Character {
+        private String name;
+        private double health;
+        private Weapon currentWeapon;
+
+        Character(String n, double h){
+          this.name = n;
+          this.health = h;
+        }
+
+        getDamage(double damage) {
+          this.health -= damage;
+          if (this.health < 0) {
+            this.health = 0;
+          }
+        }
+
+        takePills(double pills) {
+          this.health += pills;
+        }
+
+        setWeapon(Weapon wp) {
+            this.currentWeapon = wp;
+        }
+    }
+
+    class Worm extends Character{
+      private double speed;
+      private double protection;
+      private double jump;
+
+      Worm(String n, double h, double s, double p, double j){
+        super(n, h);
+        this.setSpeed(s);
+        this.protection = p;
+        this.jump = j;
+      }
+      
+      setSpeed(double s){
+        this.speed = s;
+      }
+
+    }
+
+    class Weapon {
+      private String name;
+      private double damage;
+      private double range;
+      private double spread;
+
+      Weapon(String n, double d, double r, double s,){
+        this.name = n;
+        this.damage = d;
+        this.range = r;
+        this.spread = s;
+      }
+
+      changeSpread(double val) {
+        this.spread += val;
+      }
+    }
+
+    class Item { 
+      private int left;
+      private int top; 
+      private String img; 
+
+      Item(int l, int t){
+        this.left = l;
+        this.top = t;
+      }
+
+      changePosition(int l, int t) {
+        this.left = l;
+        this.top = t;
+      }
+    }
+
+    class Map {
+      private int width;
+      private int height;
+      private Item[] items;
+
+      Map(int w, int h) {
+        this.width = w;
+        this.height = h;
+      }
+    }
