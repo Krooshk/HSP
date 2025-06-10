ca6820e74d6455574d79978fda7ed0373fe3ab1d
Kirill <kirill.krush@mail.ru>
2025-06-10 10:11:19 +0300

    refactor: change last test

--- jv1/1_13/src/MyCodeTest.java
+++ jv1/1_13/src/MyCodeTest.java
@@ -25,30 +25,28 @@ public class MyCodeTest {
     @Test
     public void testZero() {
         int[] arr = new int[] {0};
-        int[] secondArr = new int[] {0};
-        assertTrue(MyCode.equal(MyCode.sort(arr), secondArr));
+        int[] sortedArr = new int[] {0};
+        assertTrue(MyCode.equal(MyCode.sort(arr), sortedArr));
     }
 
     @Test
     public void testEmpty() {
         int[] arr = new int[] {};
-        int[] secondArr = new int[] {};
-        assertTrue(MyCode.equal(MyCode.sort(arr), secondArr));
+        int[] sortedArr = new int[] {};
+        assertTrue(MyCode.equal(MyCode.sort(arr), sortedArr));
     }
 
     @Test
     public void testExtremum() {
         int[] arr = new int[] { 2147483647 , -2147483648, 3, 5, 7, 9 };
-        int[] secondArr = new int[] { -2147483648, 3, 5, 7, 9, 2147483647  };
-        assertTrue(MyCode.equal(MyCode.sort(arr), secondArr));
+        int[] sortedArr = new int[] { -2147483648, 3, 5, 7, 9, 2147483647  };
+        assertTrue(MyCode.equal(MyCode.sort(arr), sortedArr));
     }
 
     @Test
     public void testRegress() {
         int[] arr = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-        int[] copyArr = MyCode.copy(arr);
-        Arrays.sort(copyArr);
-        assertTrue(MyCode.equal(MyCode.sort(arr), copyArr));
+        int[] sortedArr = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertTrue(MyCode.equal(MyCode.sort(arr), sortedArr));
     }
-
 }
