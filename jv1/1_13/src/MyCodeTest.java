d036640b068c1e4014f4baa259adeee6a784038f
Kirill <kirill.krush@mail.ru>
2025-06-10 10:12:46 +0300

    style: add spaces

--- jv1/1_13/src/MyCodeTest.java
+++ jv1/1_13/src/MyCodeTest.java
@@ -13,7 +13,7 @@ public class MyCodeTest {
 
         int size = 1000;
         int[] arr = new int[size];
-        for (int i = 0; i < size; i ++) {
+        for (int i = 0; i < size; i++) {
             arr[i] = rand.nextInt(size);
         }
 
@@ -24,8 +24,8 @@ public class MyCodeTest {
 
     @Test
     public void testZero() {
-        int[] arr = new int[] {0};
-        int[] sortedArr = new int[] {0};
+        int[] arr = new int[] { 0 };
+        int[] sortedArr = new int[] { 0 };
         assertTrue(MyCode.equal(MyCode.sort(arr), sortedArr));
     }
 
@@ -39,14 +39,14 @@ public class MyCodeTest {
     @Test
     public void testExtremum() {
         int[] arr = new int[] { 2147483647 , -2147483648, 3, 5, 7, 9 };
-        int[] sortedArr = new int[] { -2147483648, 3, 5, 7, 9, 2147483647  };
+        int[] sortedArr = new int[] { -2147483648, 3, 5, 7, 9, 2147483647 };
         assertTrue(MyCode.equal(MyCode.sort(arr), sortedArr));
     }
 
     @Test
     public void testRegress() {
-        int[] arr = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
-        int[] sortedArr = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        int[] sortedArr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         assertTrue(MyCode.equal(MyCode.sort(arr), sortedArr));
     }
 }
