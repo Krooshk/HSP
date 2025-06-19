f05df372c19ba187295706ce18a8e9c4df62b0bb
Kirill <kirill.krush@mail.ru>
2025-06-19 18:52:40 +0300

    task 18: add solution and reflection

---
+++ jv1/1_18/src/Main.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
+public class Main {
+    // Задание 1
+    public static void operationWithMaps(){
+        HashMap<Integer, String> map = new HashMap<>();
+
+        int[] array = getRandomArray();
+        int[] array2 = getRandomArray();
+
+        for (int i = 0; i < 100; i++){
+            map.put(array[i], String.valueOf(array2[i]));
+        }
+
+        for (Integer key : map.keySet()) {
+            System.out.println(map.get(key));
+        }
+
+        map.clear();
+    }
+
+    public static int[] getRandomArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < 100; i++){
+            array[i] = i + 1;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i; j < array.length - 1; j++) {
+                Random rand = new Random();
+                boolean willSwap = rand.nextInt(2) == 0;
+
+                if (willSwap) {
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+
+        return array;
+    }
+
+    // Задание 2
+    public static ArrayList<Integer> getNumsMoreThen(ArrayList<Integer> list, int N) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int num : list) {
+            if (map.containsKey(num)) {
+                int old = map.get(num);
+                map.put(num, old + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        for (Integer key : map.keySet()) {
+            if (map.get(key) >= N) {
+                result.add(key);
+            }
+        }
+
+        map.clear();
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // Вызов для задания 1
+        operationWithMaps();
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < 100; i++){
+            Random rand = new Random();
+            int num = rand.nextInt(10) + 1;
+            list.add(num);
+        }
+
+        System.out.println("_________________");
+
+        // Получение списка из задания 2
+        ArrayList<Integer> res = getNumsMoreThen(list, 12);
+        for (Integer val : res) {
+            System.out.println(val);
+        }
+    }
+}
