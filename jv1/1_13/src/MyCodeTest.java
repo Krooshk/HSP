f37ee5f9f0e8d060cc96e28f72706e643cf95329
Kirill <kirill.krush@mail.ru>
2025-06-24 21:36:55 +0300

    install junit5 and repeatedTest

--- jv1/1_13/src/MyCodeTest.java
+++ jv1/1_13/src/MyCodeTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -7,7 +8,7 @@ import static org.junit.Assert.assertTrue;
 
 public class MyCodeTest {
 
-    @Test
+    @RepeatedTest(100)
     public void testRandom() {
         Random rand = new Random();
 
@@ -45,7 +46,7 @@ public class MyCodeTest {
 
     @Test
     public void testRegress() {
-        int[] arr = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        int[] arr = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 , 0};
         int[] sortedArr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         assertTrue(MyCode.equal(MyCode.sort(arr), sortedArr));
     }
