068b7e4809b443bac08589aacf1afff14000a11f
Kirill <kirill.krush@mail.ru>
2025-07-03 22:40:51 +0300

    task6: refactor change logic

--- 28tasks/6/Task6Test.java
+++ 28tasks/6/Task6Test.java
@@ -6,26 +6,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Task6Test {
     @Test public void testMinDirectly() {
-        int[] hits = new int[]{ 7, 8 };
+        int[] hits = new int[]{ 1, 2 };
         assertEquals(Task6.PatternUnlock(2, hits), "1");
     }
 
     @Test public void testMinDiagonal() {
-        int[] hits = new int[]{ 7, 2 };
+        int[] hits = new int[]{ 1, 5 };
         assertEquals(Task6.PatternUnlock(2, hits), "141421");
     }
 
     @Test public void testInt() {
-        int[] hits = new int[]{ 6, 1, 9, 8, 7, 3, 4, 5, 2 };
-        assertEquals(Task6.PatternUnlock(10, hits), "8");
-    }
-
-    @Test public void testRegres() {
-        int[] hits = new int[]{ 1, 2, 3, 4, 5, 6, 2, 7, 8, 9, };
-        assertEquals(Task6.PatternUnlock(10, hits), "982843");
+        int[] hits = new int[]{ 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+        assertEquals(Task6.PatternUnlock(9, hits), "8");
     }
 
     @Test public void testRemoveZero(){
         assertEquals(Task6.removeZero("10.012"), "112");
+    }
+
+    @Test public void testRegres() {
+        int[] hits = new int[]{ 2, 5, 8, 7, 4, 1, 9, 6, 3 };
+        assertEquals(Task6.PatternUnlock(9, hits), "982843");
+    }
+
+    @Test public void testRegres2(){
+        int[] hits = new int[]{ 2, 1, 9, };
+        assertEquals(Task6.PatternUnlock(3, hits), "382843");
     }
 }
