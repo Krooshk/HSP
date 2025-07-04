41b23ee88a59c04c1354f066bac98053b11b7577
Kirill <kirill.krush@mail.ru>
2025-07-04 09:40:09 +0300

    task6: return first variant of solution

--- 28tasks/6/Task6Test.java
+++ 28tasks/6/Task6Test.java
@@ -11,12 +11,12 @@ public class Task6Test {
     }
 
     @Test public void testMinDiagonal() {
-        int[] hits = new int[]{ 1, 5 };
+        int[] hits = new int[]{ 1, 8 };
         assertEquals(Task6.PatternUnlock(2, hits), "141421");
     }
 
     @Test public void testInt() {
-        int[] hits = new int[]{ 1, 2, 3, 6, 9, 8, 7, 4, 5 };
+        int[] hits = new int[]{ 6, 1, 9, 8, 7, 3, 4, 5, 2 };
         assertEquals(Task6.PatternUnlock(9, hits), "8");
     }
 
@@ -25,12 +25,13 @@ public class Task6Test {
     }
 
     @Test public void testRegres() {
-        int[] hits = new int[]{ 2, 5, 8, 7, 4, 1, 9, 6, 3 };
-        assertEquals(Task6.PatternUnlock(9, hits), "982843");
+
+        int[] hits = new int[]{ 1, 2, 3, 4, 5, 6, 2, 7, 8, 9 };
+        assertEquals(Task6.PatternUnlock(10, hits), "982843");
     }
 
     @Test public void testRegres2(){
         int[] hits = new int[]{ 2, 1, 9, };
-        assertEquals(Task6.PatternUnlock(3, hits), "382843");
+        assertEquals(Task6.PatternUnlock(3, hits), "2");
     }
 }
--- 28tasks/6/for_server/another_task6.java
+++ 28tasks/6/for_server/another_task6.java
@@ -3,15 +3,26 @@ import java.util.*;
 public class Level1
 {
     public static String PatternUnlock(int N, int [] hits) {
+        HashMap<Integer, Integer[]> oneHit = new HashMap<>();
+        oneHit.put(1, new Integer[]{ 2, 6, 9});
+        oneHit.put(2, new Integer[]{ 1, 5, 3, 8});
+        oneHit.put(3, new Integer[]{ 2, 4, 7});
+        oneHit.put(4, new Integer[]{ 5, 3 });
+        oneHit.put(5, new Integer[]{ 6, 2, 4} );
+        oneHit.put(6, new Integer[]{ 1, 5 });
+        oneHit.put(7, new Integer[]{ 3, 8 });
+        oneHit.put(8, new Integer[]{ 2, 7, 9});
+        oneHit.put(9, new Integer[]{ 1, 8 });
+
         double diag = Math.sqrt(2);
         int prev = hits[0];
         double res = 0;
 
-        for (int i = 1; i < hits.length; i++) {
-            if (Math.abs(hits[i] - prev) == 3 || Math.abs(hits[i] - prev) == 1) {
+        for (int i = 1; i < N; i++) {
+            Integer[] neighbours = oneHit.get(hits[i]);
+            List<Integer> list = Arrays.asList(neighbours);
+            if (list.contains(prev)) {
                 res += 1.0;
-            } else if (Math.abs(hits[i] - prev) == 8 || (prev + hits[i] == 10) && Math.abs(hits[i] - prev) == 4) {
-                res += 2 * diag;
             } else {
                 res += diag;
             }
