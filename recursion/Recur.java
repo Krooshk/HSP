679510b3cb33ae1efe864e0ecf7f83d22909f91b
Kirill <kirill.krush@mail.ru>
2025-07-27 12:12:04 +0300

    recursion: task 3-4

--- recursion/Recur.java
+++ recursion/Recur.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 class Recur {
     public static int degree(int N, int M) {
@@ -17,5 +18,28 @@ class Recur {
 
         return remainder + sumOfDigit(main);
     }
+
+    public static int getLengthOfList(LinkedList<String> list){
+        if (list.size() == 0) {
+            return 0;
+        }
+        list.pop();
+
+        return 1 + getLengthOfList(list);
+    }
+
+    public static boolean isPalindrome(String str){
+        int end = str.length() - 1;
+        if (str.length() <= 1) {
+            return true;
+        }
+
+        if (str.charAt(0) != str.charAt(end)){
+            return false;
+        }
+
+        return isPalindrome(str.substring(1, end));
+    }
+
 
 }
--- recursion/RecurTest.java
+++ recursion/RecurTest.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RecurTest {
@@ -30,4 +34,36 @@ public class RecurTest {
     @Test public void testSumOfDigitRegres4(){
         assertEquals(Recur.sumOfDigit(11111), 5);
     }
+
+    @RepeatedTest(10) public void testGetLengthOfList(){
+        LinkedList<String> list = new LinkedList<>();
+        Random rand = new Random();
+        int N = rand.nextInt(20) + 1;
+
+        for (int i = 0; i < N; i++){
+            list.add("q");
+        }
+        assertEquals(Recur.getLengthOfList(list), N);
+    }
+
+    @Test public void testPalindromeRegresOdd(){
+        assertEquals(Recur.isPalindrome("madam"), true);
+    }
+
+    @Test public void testPalindromeRegresEven(){
+        assertEquals(Recur.isPalindrome("abccba"), true);
+    }
+
+    @Test public void testPalindromeRegresOddFalse(){
+        assertEquals(Recur.isPalindrome("qwert"), false);
+    }
+
+    @Test public void testPalindromeRegresEvenFalse(){
+        assertEquals(Recur.isPalindrome("balzam"), false);
+    }
+
+    @Test public void testPalindromeEmpty(){
+        assertEquals(Recur.isPalindrome(""), true);
+    }
+
 }
