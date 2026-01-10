10b4df6394a96212f792d9d8be237777ce2093a1
Kirill <kirill.krush@mail.ru>
2026-01-10 21:39:32 +0300

    finish additional task 1,4

---
+++ ASD2/2_BST/BSTAdditional.java
@@ -0,0 +1,226 @@
+import java.io.*;
+import java.util.*;
+
+
+class BSTAdditional<T>
+{
+    BSTNode<T> Root; // корень дерева, или null
+
+    public BSTAdditional(BSTNode<T> node)
+    {
+        Root = node;
+    }
+
+    // FindNodeByKey, Time - O(logN), Space - O(1)
+    public BSTFind<T> FindNodeByKey(int key)
+    {
+
+        if (Root == null) {
+            return null;
+        }
+
+        return traverseFindNodeByKey(key, Root);
+    }
+
+    private BSTFind<T> traverseFindNodeByKey(int key, BSTNode<T> node){
+        if (node.NodeKey == key) {
+            BSTFind<T> bstFind = new BSTFind<>();
+            bstFind.Node = node;
+            bstFind.NodeHasKey = true;
+            return bstFind;
+        }
+
+        if (key < node.NodeKey && node.LeftChild == null) {
+            BSTFind<T> bstFind = new BSTFind<>();
+            bstFind.Node = node;
+            bstFind.NodeHasKey = false;
+            bstFind.ToLeft = true;
+            return bstFind;
+        }
+
+        if (key > node.NodeKey && node.RightChild == null) {
+            BSTFind<T> bstFind = new BSTFind<>();
+            bstFind.Node = node;
+            bstFind.NodeHasKey = false;
+            bstFind.ToLeft = false;
+            return bstFind;
+        }
+
+        BSTFind<T> result = key < node.NodeKey ? traverseFindNodeByKey(key, node.LeftChild) : traverseFindNodeByKey(key, node.RightChild);
+        return result;
+
+    }
+
+    // AddKeyValue, Time - O(logN), Space - O(1)
+    public boolean AddKeyValue(int key, T val)
+    {
+        BSTFind<T> interimResultFindKey = FindNodeByKey(key);
+        if (interimResultFindKey.NodeHasKey) return false;
+        BSTNode<T> newNode = new BSTNode<>(key, val, interimResultFindKey.Node);
+
+        if (interimResultFindKey.ToLeft) {
+            interimResultFindKey.Node.LeftChild = newNode;
+            return true;
+        }
+
+        interimResultFindKey.Node.RightChild = newNode;
+        return true;
+    }
+
+    // FinMinMax, Time - O(logN), Space - O(1)
+    public BSTNode<T> FinMinMax(BSTNode<T> FromNode, boolean FindMax)
+    {
+        if (FromNode == null) return null;
+
+        return traverseExtremum(FindMax, FromNode);
+    }
+
+    private BSTNode<T> traverseExtremum(boolean isMax, BSTNode<T> node) {
+        if (isMax && node.RightChild == null){
+            return node;
+        }
+
+        if (!isMax && node.LeftChild == null){
+            return node;
+        }
+
+        if (isMax) {
+            return traverseExtremum(isMax, node.RightChild);
+        }
+
+        return traverseExtremum(isMax, node.LeftChild);
+    }
+
+    // DeleteNodeByKey, Time - O(logN), Space - O(1)
+    public boolean DeleteNodeByKey(int key)
+    {
+        BSTFind<T> interimResultFindKey = FindNodeByKey(key);
+        BSTNode<T> toDeletedNode = interimResultFindKey.Node;
+        if (!interimResultFindKey.NodeHasKey) return false;
+
+        boolean isHasRightChild = toDeletedNode.RightChild != null;
+        boolean isHasLeftChild = toDeletedNode.LeftChild != null;
+        boolean isHasOneChild = ((isHasRightChild && !isHasLeftChild) || (!isHasRightChild && isHasLeftChild));
+
+        if (isHasOneChild) {
+            BSTNode<T> child = toDeletedNode.RightChild != null ? toDeletedNode.RightChild : toDeletedNode.LeftChild;
+            connectParentNode(child, toDeletedNode.Parent);
+            return true;
+        }
+
+        BSTNode<T> replacement = FinMinMax(toDeletedNode.RightChild, false);
+        connectParentNode(replacement.RightChild, replacement.Parent);
+
+        replacement.RightChild = toDeletedNode.RightChild == replacement ? null : toDeletedNode.RightChild;
+        replacement.LeftChild = toDeletedNode.LeftChild == replacement ? null : toDeletedNode.LeftChild;
+
+        removeConnectionWithParent(replacement);
+        connectParentNode(replacement, toDeletedNode.Parent);
+
+        return true;
+    }
+
+    private void removeConnectionWithParent(BSTNode<T> node){
+        if (node.Parent.NodeKey > node.NodeKey) {
+            node.Parent.LeftChild = null;
+            return;
+        }
+
+        node.Parent.RightChild = null;
+    }
+
+    private void connectParentNode(BSTNode<T> node, BSTNode<T> parent){
+        if (node == null) return;
+
+        node.Parent = parent;
+
+        if (parent == null) {
+            Root = node;
+            return;
+        }
+        if (parent.NodeKey > node.NodeKey) {
+            node.Parent.LeftChild = node;
+            return;
+        }
+        node.Parent.RightChild = node;
+    }
+
+    // Count, Time - O(N), Space - O(1)
+    public int Count()
+    {
+        return traverse(Root);
+    }
+
+    private int traverse(BSTNode<T> node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int left = traverse(node.LeftChild);
+        int right = traverse(node.RightChild);
+
+        return 1 + left + right;
+    }
+
+    // Задание 1, isIdentical, Time - O(N), Space - O(1)
+    public boolean isIdentical(BSTNode<T> node) {
+        return traverseTwoNode(Root, node);
+    }
+
+    public boolean traverseTwoNode(BSTNode<T> node1, BSTNode<T> node2) {
+        if ((node1 == null && node2 != null) || (node2 == null && node1 != null)) {
+            return false;
+        }
+        boolean isIdentical = node1.NodeKey == node2.NodeKey && node1.NodeValue == node2.NodeValue;
+
+        if (!isIdentical) {
+            return false;
+        }
+        boolean isLeaf1 = node1.LeftChild == null && node1.RightChild == null;
+        boolean isLeaf2 = node2.LeftChild == null && node2.RightChild == null;
+        boolean left = true;
+        boolean right = true;
+
+        if (isLeaf1 &&  isLeaf2 && isIdentical) {
+            return true;
+        }
+
+        if (node1.LeftChild != null) {
+            left = traverseTwoNode(node1.LeftChild, node2.LeftChild);
+        }
+
+        if (node1.RightChild != null) {
+            right = traverseTwoNode(node1.RightChild, node2.RightChild);
+        }
+
+        return left && right;
+    }
+
+    // Задание 4, isSymmetric, Time - O(N), Space - O(1)
+    public boolean isSymmetric() {
+        return traverseSym(Root.LeftChild, Root.RightChild);
+    }
+
+    public boolean traverseSym(BSTNode<T> left, BSTNode<T> right) {
+        if ((left == null && right != null) || (left == null && right != null)) {
+            return false;
+        }
+
+        boolean leftPath = true;
+        boolean rightPath = true;
+
+        if (left.RightChild == null && left.RightChild == null && right.RightChild == null && right.LeftChild == null) {
+            return true;
+        }
+
+        if (left.LeftChild != null) {
+            leftPath = traverseTwoNode(left.LeftChild, right.RightChild);
+        }
+
+        if (left.RightChild != null) {
+            rightPath = traverseTwoNode(left.RightChild, right.LeftChild);
+        }
+
+        return leftPath && rightPath;
+    }
+}
---
+++ ASD2/2_BST/BSTAdditionalTest.java
@@ -0,0 +1,127 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BSTAdditionalTest {
+    @Test public void identicalPositive(){
+        BSTNode<Integer> root = new BSTNode<Integer>(8, 8, null);
+        BSTNode<Integer> two1 = new BSTNode<Integer>(4, 4, root);
+        BSTNode<Integer> two2 = new BSTNode<Integer>(10, 10, root);
+        BSTNode<Integer> three1 = new BSTNode<Integer>(3, 3, two1);
+        BSTNode<Integer> three2 = new BSTNode<Integer>(5, 5, two1);
+        BSTNode<Integer> three3 = new BSTNode<Integer>(9, 9, two2);
+        BSTNode<Integer> three4 = new BSTNode<Integer>(12, 12, two2);
+        BSTNode<Integer> four1 = new BSTNode<Integer>(1, 1, three1);
+        BSTNode<Integer> four4 = new BSTNode<Integer>(6, 6, three2);
+        root.LeftChild = two1;
+        root.RightChild = two2;
+        two1.LeftChild = three1;
+        two1.RightChild = three2;
+        two2.LeftChild = three3;
+        two2.RightChild = three4;
+        three1.LeftChild = four1;
+        three2.RightChild = four4;
+        BSTAdditional<Integer> bst = new BSTAdditional<>(root);
+
+        BSTNode<Integer> second_root = new BSTNode<Integer>(8, 8, null);
+        BSTNode<Integer> second_two1 = new BSTNode<Integer>(4, 4, root);
+        BSTNode<Integer> second_two2 = new BSTNode<Integer>(10, 10, root);
+        BSTNode<Integer> second_three1 = new BSTNode<Integer>(3, 3, two1);
+        BSTNode<Integer> second_three2 = new BSTNode<Integer>(5, 5, two1);
+        BSTNode<Integer> second_three3 = new BSTNode<Integer>(9, 9, two2);
+        BSTNode<Integer> second_three4 = new BSTNode<Integer>(12, 12, two2);
+        BSTNode<Integer> second_four1 = new BSTNode<Integer>(1, 1, three1);
+        BSTNode<Integer> second_four4 = new BSTNode<Integer>(6, 6, three2);
+        second_root.LeftChild = second_two1;
+        second_root.RightChild = second_two2;
+        second_two1.LeftChild = second_three1;
+        second_two1.RightChild = second_three2;
+        second_two2.LeftChild = second_three3;
+        second_two2.RightChild = second_three4;
+        second_three1.LeftChild = second_four1;
+        second_three2.RightChild = second_four4;
+
+        assertTrue(bst.isIdentical(second_root));
+    }
+
+    @Test public void identicalNegative(){
+        BSTNode<Integer> root = new BSTNode<Integer>(8, 8, null);
+        BSTNode<Integer> two1 = new BSTNode<Integer>(4, 4, root);
+        BSTNode<Integer> two2 = new BSTNode<Integer>(10, 10, root);
+        BSTNode<Integer> three1 = new BSTNode<Integer>(3, 3, two1);
+        BSTNode<Integer> three2 = new BSTNode<Integer>(5, 5, two1);
+        BSTNode<Integer> three3 = new BSTNode<Integer>(9, 9, two2);
+        BSTNode<Integer> three4 = new BSTNode<Integer>(12, 12, two2);
+        BSTNode<Integer> four1 = new BSTNode<Integer>(1, 1, three1);
+        BSTNode<Integer> four4 = new BSTNode<Integer>(6, 6, three2);
+        root.LeftChild = two1;
+        root.RightChild = two2;
+        two1.LeftChild = three1;
+        two1.RightChild = three2;
+        two2.LeftChild = three3;
+        two2.RightChild = three4;
+        three1.LeftChild = four1;
+        three2.RightChild = four4;
+        BSTAdditional<Integer> bst = new BSTAdditional<>(root);
+
+        BSTNode<Integer> second_root = new BSTNode<Integer>(8, 8, null);
+        BSTNode<Integer> second_two1 = new BSTNode<Integer>(4, 4, root);
+        BSTNode<Integer> second_two2 = new BSTNode<Integer>(10, 10, root);
+        BSTNode<Integer> second_three1 = new BSTNode<Integer>(3, 3, two1);
+        BSTNode<Integer> second_three2 = new BSTNode<Integer>(5, 5, two1);
+        BSTNode<Integer> second_three3 = new BSTNode<Integer>(9, 9, two2);
+        second_root.LeftChild = second_two1;
+        second_root.RightChild = second_two2;
+        second_two1.LeftChild = second_three1;
+        second_two1.RightChild = second_three2;
+        second_two2.LeftChild = second_three3;
+
+        assertFalse(bst.isIdentical(second_root));
+    }
+
+    @Test public void isSymmetricNegative(){
+        BSTNode<Integer> root = new BSTNode<Integer>(8, 8, null);
+        BSTNode<Integer> two1 = new BSTNode<Integer>(4, 4, root);
+        BSTNode<Integer> two2 = new BSTNode<Integer>(10, 10, root);
+        BSTNode<Integer> three1 = new BSTNode<Integer>(3, 3, two1);
+        BSTNode<Integer> three2 = new BSTNode<Integer>(5, 5, two1);
+        BSTNode<Integer> three3 = new BSTNode<Integer>(9, 9, two2);
+        BSTNode<Integer> three4 = new BSTNode<Integer>(12, 12, two2);
+        BSTNode<Integer> four1 = new BSTNode<Integer>(1, 1, three1);
+        BSTNode<Integer> four4 = new BSTNode<Integer>(6, 6, three2);
+        root.LeftChild = two1;
+        root.RightChild = two2;
+        two1.LeftChild = three1;
+        two1.RightChild = three2;
+        two2.LeftChild = three3;
+        two2.RightChild = three4;
+        three1.LeftChild = four1;
+        three2.RightChild = four4;
+        BSTAdditional<Integer> bst = new BSTAdditional<>(root);
+
+        assertFalse(bst.isSymmetric());
+    }
+
+    @Test public void isSymmetricPositive(){
+        BSTNode<Integer> root = new BSTNode<Integer>(8, 8, null);
+        BSTNode<Integer> two1 = new BSTNode<Integer>(4, 4, root);
+        BSTNode<Integer> two2 = new BSTNode<Integer>(10, 10, root);
+        BSTNode<Integer> three1 = new BSTNode<Integer>(3, 3, two1);
+        BSTNode<Integer> three2 = new BSTNode<Integer>(5, 5, two1);
+        BSTNode<Integer> three3 = new BSTNode<Integer>(9, 9, two2);
+        BSTNode<Integer> three4 = new BSTNode<Integer>(12, 12, two2);
+        BSTNode<Integer> four1 = new BSTNode<Integer>(1, 1, three1);
+        BSTNode<Integer> four8 = new BSTNode<Integer>(13, 13, three4);
+        root.LeftChild = two1;
+        root.RightChild = two2;
+        two1.LeftChild = three1;
+        two1.RightChild = three2;
+        two2.LeftChild = three3;
+        two2.RightChild = three4;
+        three1.LeftChild = four1;
+        three4.RightChild = four8;
+        BSTAdditional<Integer> bst = new BSTAdditional<>(root);
+
+        assertFalse(bst.isSymmetric());
+    }
+}
