918f4f5849d97018bb9239f3160064eaebd37d9d
Kirill <kirill.krush@mail.ru>
2026-01-29 18:47:26 +0300

    finish main task for 6, left couple tests

--- ASD2/6_GENERATE_BST_II/BalancedBST.java
+++ ASD2/6_GENERATE_BST_II/BalancedBST.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 class BSTNode
 {
-    public int NodeKey; // ключ узла
-    public BSTNode Parent; // родитель или null для корня
-    public BSTNode LeftChild; // левый потомок
-    public BSTNode RightChild; // правый потомок
-    public int     Level; // глубина узла
+    public int NodeKey;
+    public BSTNode Parent;
+    public BSTNode LeftChild;
+    public BSTNode RightChild;
+    public int     Level;
 
     public BSTNode(int key, BSTNode parent)
     {
@@ -19,13 +19,14 @@ class BSTNode
 
 class BalancedBST
 {
-    public BSTNode Root; // корень дерева
+    public BSTNode Root;
 
     public BalancedBST()
     {
         Root = null;
     }
 
+    // Task 1 , Time - O(N), Space - O(N)
     public void GenerateTree(int[] a)
     {
         int[] copyArray = Arrays.copyOf(a, a.length);
@@ -50,7 +51,7 @@ class BalancedBST
         return node;
     }
 
-    // Для тестирования
+    // For test
     ArrayList<Integer> WideAllNodes() {
         ArrayList<Integer> list = new ArrayList<>();
         List<BSTNode> workList = new ArrayList<>();
@@ -67,8 +68,59 @@ class BalancedBST
         return list;
     }
 
+    // Task 2* , Time - O(N), Space - O(1)
+    public boolean IsProperBST(BSTNode root_node) {
+        ArrayList<Boolean> checks = new ArrayList<Boolean>();
+        if (root_node == null) {
+            return true;
+        }
+
+        boolean isExistRight = root_node.RightChild != null;
+        boolean isExistLeft = root_node.LeftChild != null;
+        boolean isNodeBiggerRight = isExistRight && root_node.NodeKey > root_node.RightChild.NodeKey;
+        boolean isLeftBiggerOrEqNode = isExistLeft && root_node.NodeKey <= root_node.LeftChild.NodeKey;
+
+        if (isNodeBiggerRight || isLeftBiggerOrEqNode) {
+            return false;
+        }
+
+        checks.add(IsProperBST(root_node.LeftChild));
+        checks.add(IsProperBST(root_node.RightChild));
+
+        return checks.stream().filter(el -> !el).toArray().length == 0;
+    };
+
+    // Task 3* ,
     public boolean IsBalanced(BSTNode root_node)
     {
-        return false; // сбалансировано ли дерево с корнем root_node
+        if (root_node == null) return true;
+
+        int left = root_node.LeftChild != null ? maxLevel(root_node.LeftChild) : root_node.Level;
+        int right = root_node.RightChild != null ? maxLevel(root_node.RightChild): root_node.Level;
+        if (Math.abs(right - left) >= 2) {
+            return false;
+        }
+
+        boolean isLeftBalance = IsBalanced(root_node.LeftChild);
+        boolean isRightBalance = IsBalanced(root_node.RightChild);
+
+        return isLeftBalance && isRightBalance;
+    }
+
+    Integer maxLevel(BSTNode node) {
+        Integer max = node.Level;
+        if (node.LeftChild == null && node.RightChild == null) {
+            return max;
+        }
+
+        if (node.LeftChild != null) {
+            max = Math.max(max, maxLevel(node.LeftChild));
+        }
+
+        if (node.RightChild != null) {
+            max = Math.max(max, maxLevel(node.RightChild));
+        }
+
+        return max;
     }
 }
--- ASD2/6_GENERATE_BST_II/BalancedBSTTest.java
+++ ASD2/6_GENERATE_BST_II/BalancedBSTTest.java
@@ -6,7 +6,29 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BalancedBSTTest {
-    @Test public void test(){
+    @Test public void testEmpty(){
+        BalancedBST bst = new BalancedBST();
+        int[] array = { };
+        bst.GenerateTree(array);
+
+        Integer[] result = { };
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(result));
+        assertEquals(bst.WideAllNodes(), expected);
+        assertTrue(bst.IsProperBST(bst.Root));
+    }
+
+    @Test public void testOne(){
+        BalancedBST bst = new BalancedBST();
+        int[] array = { 1 };
+        bst.GenerateTree(array);
+
+        Integer[] result = { 1 };
+        ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(result));
+        assertEquals(bst.WideAllNodes(), expected);
+        assertTrue(bst.IsProperBST(bst.Root));
+    }
+
+    @Test public void testRegres(){
         BalancedBST bst = new BalancedBST();
         int[] array = { 1, 3, 5, 7, 6, 4, 2 };
         bst.GenerateTree(array);
@@ -14,5 +36,44 @@ public class BalancedBSTTest {
         Integer[] result = { 4, 2, 6, 1, 3, 5, 7 };
         ArrayList<Integer> expected = new ArrayList<>(Arrays.asList(result));
         assertEquals(bst.WideAllNodes(), expected);
+        assertTrue(bst.IsProperBST(bst.Root));
+    }
+
+    @Test public void testNegativeProper(){
+        BalancedBST bst = new BalancedBST();
+        int[] array = { 1 };
+        bst.GenerateTree(array);
+        BSTNode left = new BSTNode(5, bst.Root);
+        bst.Root.LeftChild = left;
+
+        assertFalse(bst.IsProperBST(bst.Root));
+    }
+
+    @Test public void testNegativeProperEq(){
+        BalancedBST bst = new BalancedBST();
+        int[] array = { 1 };
+        bst.GenerateTree(array);
+        BSTNode left = new BSTNode(1, bst.Root);
+        bst.Root.LeftChild = left;
+
+        assertFalse(bst.IsProperBST(bst.Root));
+    }
+
+    @Test public void testPositiveProperEq(){
+        BalancedBST bst = new BalancedBST();
+        int[] array = { 1 };
+        bst.GenerateTree(array);
+        BSTNode right = new BSTNode(1, bst.Root);
+        bst.Root.RightChild = right;
+
+        assertTrue(bst.IsProperBST(bst.Root));
+    }
+
+    @Test public void testRegresBalanced(){
+        BalancedBST bst = new BalancedBST();
+        int[] array = { 1, 3, 5, 7, 6, 4, 2 };
+        bst.GenerateTree(array);
+
+        assertTrue(bst.IsBalanced(bst.Root));
     }
 }
