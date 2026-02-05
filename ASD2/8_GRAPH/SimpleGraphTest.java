24d3fa87300e306b6e66ef0ca023b56ba008bf15
Kirill <kirill.krush@mail.ru>
2026-02-05 15:33:07 +0300

    done 1/2 of 8 task

---
+++ ASD2/8_GRAPH/SimpleGraphTest.java
@@ -0,0 +1,80 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleGraphTest {
+    @Test public void testAddVertex(){
+        SimpleGraph sg = new SimpleGraph(3);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+
+        assertEquals(sg.vertex[0].Value, 33);
+        assertEquals(sg.vertex[1].Value, 33);
+        assertEquals(sg.vertex[2].Value, 33);
+    }
+
+    @Test public void testAddVertexSize(){
+        SimpleGraph sg = new SimpleGraph(3);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+
+        assertEquals(sg.vertex.length, 3);
+    }
+
+    @Test public void testAdjacency(){
+        SimpleGraph sg = new SimpleGraph(3);
+        int[][] adj = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        assertArrayEquals(sg.m_adjacency, adj);
+
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+
+        assertArrayEquals(sg.m_adjacency, adj);
+    }
+
+    @Test public void testAdjacencyAfterAddEdge(){
+        SimpleGraph sg = new SimpleGraph(3);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        int[][] adjBefore = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+        assertArrayEquals(sg.m_adjacency, adjBefore);
+
+        sg.AddEdge(0, 1);
+        sg.AddEdge(0, 2);
+
+        int[][] adjAfter = {{0, 1, 1}, {1, 0, 0}, {1, 0, 0}};
+        assertArrayEquals(sg.m_adjacency, adjAfter);
+    }
+
+    @Test public void testAdjacencyAfterRemoveEdge(){
+        SimpleGraph sg = new SimpleGraph(3);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        sg.AddEdge(0, 1);
+        sg.AddEdge(0, 2);
+
+        sg.RemoveEdge(0, 2);
+
+        int[][] adj = {{0, 1, 0}, {1, 0, 0}, {0, 0, 0}};
+        assertArrayEquals(sg.m_adjacency, adj);
+    }
+
+    @Test public void testAdjacencyAfterRemoveVertex(){
+        SimpleGraph sg = new SimpleGraph(3);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        sg.AddVertex(33);
+        sg.AddEdge(0, 1);
+        sg.AddEdge(0, 2);
+
+        sg.RemoveVertex(1);
+
+        int[][] adj = {{0, 0, 1}, {0, 0, 0}, {1, 0, 0}};
+        assertArrayEquals(sg.m_adjacency, adj);
+    }
+}
