d02d6336aaf7abf5f55675b0354b2c3a176c2985
Krooshk <98127179+Krooshk@users.noreply.github.com>
2026-03-10 18:46:14 +0300

    Create two.sql

---
+++ SQL/two.sql
@@ -0,0 +1,9 @@
+-- Занятие 2
+-- EmployeeTerritories - один-к-одному (запись определенной территории только одна).
+-- Territories - многие-ко-многим(territoryId из другой таблицы и связь с Region).
+-- Products - один-ко-многим (связь с Suppliers, Categories).
+-- Orders - многие-ко-многим(OrderDetails id из другой таблицы и связь с Employees).
+-- OrderDetails - один-ко-многим(связь с Products).
+
+-- Рефлексия - занятие 1
+-- Contacts - не упомянул, что в этой таблице все контакты вместе.
