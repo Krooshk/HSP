8b0600a654a7b763e37b78414d56fddf445b25fa
Krooshk <98127179+Krooshk@users.noreply.github.com>
2026-03-19 18:28:03 +0300

    Create nine.sql

---
+++ SQL/nine.sql
@@ -0,0 +1,30 @@
+-- 9.4. Задания
+
+-- 9.4.1. 
+--   Найдите все пары из разных (уникальных) заказчиков (Customers), 
+--   для которых не задан регион (поле Region).
+SELECT t1.ContactName, t2.ContactName
+FROM Customers t1, Customers t2
+WHERE t1.Region IS NULL AND t2.Region IS NULL
+AND (t1.CustomerID <> t2.CustomerID) 
+
+-- 9.4.2. 
+--   Найдите вложенным запросом список заказов (Orders), 
+--   в котором у заказчиков (Customers) регион не пуст (поле Region).
+SELECT * FROM Orders t1
+WHERE EXISTS 
+(SELECT * FROM Customers t2
+WHERE 
+t1.CustomerID = t2.CustomerID
+AND
+t2.Region IS NOT NULL)
+
+-- 9.4.3. 
+--   Немного условный, но показательный пример. Найдите все заказы (таблица Orders), 
+--   цена за доставку товара которых (Freight) превышает цену любого товара (поле UnitPrice, таблица Products).
+SELECT * FROM Orders t1
+WHERE t1.Freight > ALL 
+(SELECT UnitPrice FROM Products)
+
+-- Рефлексия по заданию 8.
+-- Сделал аналогичным образом.
