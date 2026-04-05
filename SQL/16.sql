bd9348d92336898cd8387fe14d7eac2512a2bc89
Krooshk <98127179+Krooshk@users.noreply.github.com>
2026-04-05 17:36:52 +0300

    Create 16.sql

---
+++ SQL/16.sql
@@ -0,0 +1,10 @@
+-- 16 Задание
+
+USE MyTest
+CREATE CLUSTERED INDEX PK_TERRITORY ON Territories (TerritoryId)
+
+USE MyTest
+CREATE CLUSTERED INDEX PK_REGION ON Region (RegionId)
+
+-- Рефлексия по заданию 15.
+-- В уроке не было эталонного объяснения. Предполагаю, что из-за формата задачи.
