d13c4348ceaebc15d3f918dda612f33406cf51d2
Krooshk <98127179+Krooshk@users.noreply.github.com>
2026-04-08 21:35:45 +0300

    Create 18.sql

---
+++ SQL/18.sql
@@ -0,0 +1,42 @@
+-- 1. Получить информацию о всех гномах, которые входят в какой-либо отряд, вместе с информацией об их отрядах.
+SELECT Dwarves.dwarf_id, Dwarves.name , Dwarves.age, Dwarves.profession, Squads.name , Squads.mission 
+FROM Dwarves, Squads
+WHERE Dwarves.squad_id IS NOT NULL AND Dwarves.squad_id = Squads.squad_id;   
+
+-- 2. Найти всех гномов с профессией "miner", которые не состоят ни в одном отряде.
+
+SELECT * FROM Dwarves
+WHERE profession = "miner" AND squad_id IS NULL
+
+-- 3. Получить все задачи с наивысшим приоритетом, которые находятся в статусе "pending".
+
+SELECT * FROM Tasks
+WHERE priority = (SELECT Max(priority) FROM Tasks) 
+AND status = 'pending'
+
+-- 4. Для каждого гнома, который владеет хотя бы одним предметом, получить количество предметов, которыми он владеет.
+
+SELECT * FROM Dwarves, 
+WHERE dwarf_id = ANY 
+(SELECT owner_id FROM Items) 
+OR EXISTS 
+(SELECT owner_id FROM Items
+WHERE owner_id IS NULL)
+JOIN 
+(SELECT COUNT() as total FROM Items
+GROUP BY owner_id) as countItems
+ON countItems.owner_id = dwarf_id;
+
+SELECT Count() FROM Items
+GROUP BY owner_id
+HAVING owner_id IS NULL
+
+
+-- 5. Получить список всех отрядов и количество гномов в каждом отряде. Также включите в выдачу отряды без гномов.
+
+-- 6. Получить список профессий с наибольшим количеством незавершённых задач ("pending" и "in_progress") у гномов этих профессий.
+
+-- 7. Для каждого типа предметов узнать средний возраст гномов, владеющих этими предметами.
+
+-- 8. Найти всех гномов старше среднего возраста (по всем гномам в базе), которые не владеют никакими предметами.
+
