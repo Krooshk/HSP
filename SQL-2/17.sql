73d1477bd88a0d29267972c2d06423b0301f189c
Krooshk <98127179+Krooshk@users.noreply.github.com>
2026-06-05 18:32:28 +0300

    Create 17.sql

---
+++ SQL-2/17.sql
@@ -0,0 +1,4 @@
+Большую часть не выполнил.
+
+total_historical_casualties, seasonal_risk_factors, vulnerability_analysis, defense_effectiveness, military_readiness_assessment, security_evolution, recommendation_summary - не реализованы.
+active_threats - реализован.
